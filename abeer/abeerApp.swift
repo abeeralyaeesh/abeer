cf733757dff9354125dcead04092c4d2d0e221e2
AbeerAlyaeesh <abeer.alyaeesh3@gmail.com>
2023-10-05 12:01:59 +0300

    Initial Commit

---
+++ abeer/abeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  abeerApp.swift
+//  abeer
+//
+//  Created by Abeer Alyaeesh on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct abeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
